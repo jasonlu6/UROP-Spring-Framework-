82705c26d22411d6225f3289489bf35cf23713a8
Jason Lu <jasonlu6@bu.edu>
2017-06-09 16:33:31 -0400

    New files added to Clotho UI Starter
    Additional files with respect to JSP and Mongo: 
    
    index.jsp - Java Servlet Page that will be the basis of a Clotho user "profile" 
    user.jsp - the Java Servlet Page main page (right now HTML static text) 
    Mongo.java - the Java file that will create an instance of Java class that connects to mongoDB
    MongoClient.java - the Java file that will create a class that will be the middleware function between Mongo and Java
    DBCollection.java - the Java file that will allow the user to get convenient access to the testClotho database collections
    DB.java - the Java file that will connect to the mongoDB file testClotho

---
+++ DB.java
@@ -0,0 +1,20 @@
+package com.jasonlu.servlet.clotho;
+
+public class DB {
+
+	public boolean authenticate(String string, char[] charArray) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean authenticate(String string, String string2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public DBCollection getCollection(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ DBCollection.java
@@ -0,0 +1,5 @@
+package com.jasonlu.servlet.clotho;
+
+public class DBCollection {
+
+}
